c280581ac4da5ebef35bdca2458a47e4bfc889cb
pnirtep <pnirtep@gmail.com>
2020-02-13 17:12:22 +0300

    Templates+Models add

--- Blog/blueprints/films.py
+++ Blog/blueprints/films.py
@@ -1,17 +1,22 @@
 from flask import Blueprint, render_template
 from flask_login import current_user
+from sqlalchemy.orm import sessionmaker
 
+from Blog.models import Post, Base, engine
 
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
 
 film_app = Blueprint('film_app', __name__)
 
-
-
-@film_app.route('/home-alone', endpoint='home-alone')
-def page_view():
+@film_app.route('/', endpoint='films')
+def page_view(post_id):
     auth_user = current_user.is_authenticated
-    return render_template('home-alone.html', auth_user = auth_user)
+    return render_template('film_page.html', auth_user = auth_user, post_id=id)
 
-@film_app.route('/knifes-out', endpoint='knifes-out')
-def page_view():
-    pass
+@film_app.route('/<int:post_id>', endpoint='film_view')
+def film_view(post_id):
+    auth_user = current_user.is_authenticated
+    post = session.query(Post).filter_by(id=post_id).first()
+    return render_template('film-template.html', auth_user = auth_user, post_id=id, post=post)
